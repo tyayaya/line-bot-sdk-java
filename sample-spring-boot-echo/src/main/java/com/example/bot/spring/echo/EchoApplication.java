e0dcea1c3b4f9becf82b4ce40bd7164fcd434641
tyayaya <109498394+tyayaya@users.noreply.github.com>
2022-08-15 15:44:21 +0900

    add code

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -46,7 +46,7 @@ import com.fasterxml.jackson.databind.*;
 @LineMessageHandler
 public class EchoApplication {
     private final Logger log = LoggerFactory.getLogger(EchoApplication.class);
-
+    static Tokenizer tokenizer = new Tokenizer();
     public static void main(String[] args) {
         SpringApplication.run(EchoApplication.class, args);
     }
@@ -61,5 +61,250 @@ public class EchoApplication {
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
+    }
+
+    public static String generateResponse(String userInput) {
+		String responce = "";
+		ArrayList<String> names = new ArrayList<>();
+		ArrayList<String> save = new ArrayList<>();
+		List<Token> tokens = tokenizer.tokenize(userInput);
+		String before = "";
+		for(Token token: tokens) {
+			String word = token.getPartOfSpeechLevel1();
+			if(word.equals("名詞")) {
+				if(before.equals("名詞")) {
+					names.set(names.size() - 1, names.get(names.size() - 1) + token.getSurface());
+					save.add(names.get(names.size() - 1));
+					save.add(token.getSurface());
+				}
+				else {
+					names.add(token.getSurface());
+				}
+			}
+			before = word;
+		}
+		String A = names.get(0);
+		String B = names.get(1);
+		List<String> proplds = getWikidataProplds(B);
+		String wdJson = getWikidataJson(A);
+		Map<String, Object> wdMap = json2Map(wdJson);
+		List<Map<String, Object>> list = (List<Map<String, Object>>)wdMap.get("result");
+		if(list.size()==0) {
+			return A + "の意味が分かりません。";
+		}
+		else if(proplds.size()==0) {
+			return generateTrivia(A, B, list);
+		}
+		List<String> propvals = getPropVals(list.get(0), proplds.get(0));
+		if(propvals.size() == 0) {
+			return generateTrivia(A, B, list);
+		}
+		responce = getLabelById(getWikidataIds(A).get(0)) + "の" + getLabelById(proplds.get(0)) + "は";
+		for(int i=0; i<propvals.size()-1; i++) {
+			responce += propvals.get(i) + "、";
+		}
+		responce += propvals.get(propvals.size()-1) + "です。";
+		return responce;
+	}
+	
+	/**
+	 * 
+	 * @param res
+	 * @return
+	 */
+	public static String getEntityID(Map<String, Object> res) {
+		return (String)((Map)res.get("entities")).keySet().iterator().next();
+	}
+	
+	/**
+	 * 
+	 * @param map
+	 * @param prop
+	 * @return
+	 */
+	public static List<String> getPropVals(Map<String, Object> res, String prop) {
+		List<String> vals = new ArrayList<String>();
+		String entityID = getEntityID(res);
+		Map entityMap = (Map)((Map)res.get("entities")).get(entityID);
+		Map claimMap = (Map)entityMap.get("claims");
+		if (claimMap != null) {	
+			List<Map> propList = (List<Map>)claimMap.get(prop);
+			if (propList != null) {
+				for (Map propMap: propList) {
+					Map<String,Object> valMap = (Map)((Map)propMap.get("mainsnak")).get("datavalue");
+					Object val = valMap.get("value");
+					if(val instanceof String) {
+						vals.add((String)val);
+					}else if(val instanceof Map) {
+						Map map = (Map)val;
+						String id = (String)map.get("id");
+						String jaLabel = getLabelById(id);
+						vals.add(jaLabel);
+					}else {
+						vals.add(val.toString());
+					}
+					
+				}
+			}
+		}	
+		return vals;
+	}
+	
+	
+	/**
+	 * Wikidataからデータを検索
+	 * @param query
+	 * @return　Wikidataから取得したJSON文字列
+	 */
+	public static String getWikidataJson(String query) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("{\"result\":[");
+		int initLen = sb.length();
+		List<String> ids = getWikidataIds(query);
+		List<String> proplds = getWikidataProplds(query); //プロパティに対応
+		if(proplds.contains(query)) ids = proplds;
+		for (String id: ids) {
+			if (sb.length() > initLen) {
+				sb.append(",");
+			}
+			String url = "https://www.wikidata.org/wiki/Special:EntityData/" + id + ".json";
+			String json = getData(url);
+			sb.append(getData(url));
+		}
+		sb.append("]}");
+		return sb.toString();
+	}
+	
+	/**
+	 * WikidataエンティティのIDを検索
+	 * @param query
+	 * @return WikidataエンティティのIDのリスト
+	 */
+	public static List<String> getWikidataIds(String query) {
+		String encodedQuery = "";
+		try {
+			encodedQuery = URLEncoder.encode(query, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}	
+		String url = "https://www.wikidata.org/w/api.php?action=wbsearchentities&language=ja&format=json"
+		              + "&search=" + encodedQuery;
+		Map<String, Object> map = json2Map(getData(url));
+		List<Map<String, Object>> list = (List<Map<String, Object>>)map.get("search");
+		List<String> ids = new ArrayList<String>();
+		for (Map<String, Object> entMap: list) {
+			String id = (String)entMap.get("id");
+			ids.add(id);
+		}
+		return ids;
+	}
+	public static List<String> getWikidataProplds(String query) {
+		String encodedQuery = "";
+		try {
+			encodedQuery = URLEncoder.encode(query, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}	
+		String url = "https://www.wikidata.org/w/api.php?action=wbsearchentities&language=ja&format=json&type=property"
+		              + "&search=" + encodedQuery;
+		Map<String, Object> map = json2Map(getData(url));
+		List<Map<String, Object>> list = (List<Map<String, Object>>)map.get("search");
+		List<String> proplds = new ArrayList<String>();
+		for (Map<String, Object> entMap: list) {
+			String propld = (String)entMap.get("id");
+			proplds.add(propld);
+		}
+		return proplds;
+	}
+	
+	/**
+	 * JSON形式の文字列をMapに変換
+	 * @param json
+	 * @return JSONから変換したMapオブジェクト
+	 */
+	public static Map<String, Object> json2Map(String json) {
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> map = new HashMap<String, Object>();
+		map = null;
+		try {
+			map = mapper.readValue(json, new TypeReference<Map<String, Object>>(){});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return map;
+	}
+	
+	
+	/**
+	 * HTMLに限らない形式のデータをWebから取得
+	 * @param url
+	 * @return 返ってきたデータ
+	 */
+    public static String getData(String url) {
+    	String enc = "UTF-8";
+    	StringBuffer sb = new StringBuffer();
+    	try {
+    		BufferedReader in = null;
+    		if (url.startsWith("https")) {
+    			HttpsURLConnection conn = (HttpsURLConnection)new URL(url).openConnection();
+    			in = new BufferedReader(new InputStreamReader(conn.getInputStream(), enc));
+    		} else {
+    			URLConnection conn = new URL(url).openConnection();
+    			in = new BufferedReader(new InputStreamReader(conn.getInputStream(), enc));
+    		}
+    		for (String line = in.readLine(); line != null; line = in.readLine()) {
+    			sb.append(line);
+    			sb.append("\n");
+    		}
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	return sb.toString();
+    }
+    
+    public static String getLabelById(String id) {
+    	String jalabel = "";
+    	String json = getWikidataJson(id);
+    	Map<String, Object> map = json2Map(json);
+    	List<Map<String, Object>> list = (List<Map<String, Object>>)map.get("result");
+		Map entityMap = (Map)((Map)list.get(0).get("entities")).get(id);
+		Map labelMap = (Map)entityMap.get("labels");
+		jalabel = (String)((Map)labelMap.get("ja")).get("value");
+		return jalabel;
+    }
+    
+    public static List<String> getPropIds(Map<String, Object> res) {
+    	List<String> propids = new ArrayList<String>();
+    	List<String> vals = new ArrayList<String>();
+		String entityID = getEntityID(res);
+		Map entityMap = (Map)((Map)res.get("entities")).get(entityID);
+		Map claimMap = (Map)entityMap.get("claims");
+		if (claimMap != null) {	
+			propids = new ArrayList<>(claimMap.keySet());
+		}
+		return propids;
+    }
+    
+    public static String generateTrivia(String A, String B, List<Map<String, Object>> list) {
+    	String responce = "";
+    	Random rand = new Random();
+		List<String> trivia = getPropIds(list.get(0));
+		responce = getLabelById(getWikidataIds(A).get(0)) + "の" + B + "は分かりませんが、" + A + "の";
+		List<String> trivals = new ArrayList<String>();
+		while(true) {
+			int num = rand.nextInt(trivia.size());
+			String trivium = trivia.get(num);
+			try {
+				trivals = getPropVals(list.get(0), trivium);
+				responce += getLabelById(trivium) + "は";
+				break;
+			}
+			catch(Exception e){}
+		}
+		for(int i=0; i<trivals.size()-1; i++) {
+			responce += trivals.get(i) + "、";
+		}
+		responce += trivals.get(trivals.size()-1) + "です。";
+		return responce;
     }
 }
